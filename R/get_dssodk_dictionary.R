4c131eb60c3a6e5f6635e3449a43346c314d8649
Joe Brew <joebrew@gmail.com>
2016-12-09 16:39:27 +0200

    dictionary functionality

---
+++ R/get_dssodk_dictionary.R
@@ -0,0 +1,15 @@
+#' get dssodk dictionary
+#' 
+#' Get a dictionary for the following dssodk tables:
+#' HOUSEHOLD_ECONOMICS_CORE, 
+#' LOCATION_DETAILS_CORE,
+#' INDIVIDUAL_DETAILS_CORE
+#' @export
+
+get_dssodk_dictionary <- function(){
+  require(gsheet)
+  dictionary_url <- 'https://docs.google.com/spreadsheets/d/13w2ZA9zfxbr-Zc4wjooH1ScfeZfw_7_AbSY00RCd-uY/edit#gid=0'
+  dictionary <- gsheet2tbl(dictionary_url)
+  return(dictionary)
+
+}
---
+++ R/translate_dssodk.R
@@ -0,0 +1,52 @@
+#' translate dssodk
+#' 
+#' Translate columns of dssodk.
+#' @param dictionary A dictionary dataframe. Should be generated via
+#' \code{\url{get_dssodk_dictionary}}
+#' @param tab An in-memory dataframe corresponding to the \code{table_name}.
+#' Can be left \code{NULL} if \code{table_name} is identical to the name of the object in memory.
+#' @param table_name The name (as a character vector of length one) of the table
+#' to be translated
+#' @param variable The name (as a character vector of length one) of the 
+#' variable to be translated
+#' @return A one column dataframe in which the variable name will be the 
+#' Portugese language question pertaining to the \code{variable}, and
+#' the values of the dataframe will be the Portuguese-language values in the 
+#' dictionary
+#' @export
+
+translate_dssodk <- function(dictionary,
+                             tab = NULL,
+                             table_name,
+                             variable){
+  require(dplyr)
+  
+  # If no tab, try to get
+  if(is.null(tab)){
+    tab <- get(table_name, envir = .GlobalEnv)
+  }
+  
+  # Change the name of the variable so as to work smoothly with dplyr
+  the_variable <- variable
+  
+  # Select only the variable needed
+  tab <- tab %>%
+    dplyr::select_(variable)
+  # Rename to old
+  names(tab) <- 'old'
+  
+  # Subset the dictionary
+  sub_dictionary <- dictionary %>%
+    filter(table == table_name,
+           variable == the_variable) 
+  
+  # Perform the translation
+  tab <- tab %>%
+    left_join(sub_dictionary,
+              by = 'old') %>%
+    dplyr::select(answer_port) 
+  
+  names(tab) <- sub_dictionary$variable[1]
+  
+  return(tab)
+}
