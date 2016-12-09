0cb741cfd694750e874bf093fc73bd2fe154ff15
Joe Brew <joebrew@gmail.com>
2016-12-09 10:14:44 +0200

    documentation for datasets

---
+++ R/data.R
@@ -0,0 +1,276 @@
+#' moz0.
+#'
+#' Map of Mozambique at administrative level 0.
+#'
+#' @format A SpatialPolygonsDataFrame with 68 variables in the \code{data} slot : 
+#' OBJECTID
+#' ID_0
+#' ISO
+#' NAME_ENGLISH
+#' NAME_ISO
+#' NAME_FAO
+#' NAME_LOCAL
+#' NAME_OBSOLETE
+#' NAME_VARIANTS
+#' NAME_NONLATIN
+#' NAME_FRENCH
+#' NAME_SPANISH
+#' NAME_RUSSIAN
+#' NAME_ARABIC
+#' NAME_CHINESE
+#' WASPARTOF
+#' CONTAINS
+#' SOVEREIGN
+#' ISO2
+#' WWW
+#' FIPS
+#' ISON
+#' VALIDFR
+#' VALIDTO
+#' POP2000
+#' SQKM
+#' POPSQKM
+#' UNREGION1
+#' UNREGION2
+#' DEVELOPING
+#' CIS
+#' Transition
+#' OECD
+#' WBREGION
+#' WBINCOME
+#' WBDEBT
+#' WBOTHER
+#' CEEAC
+#' CEMAC
+#' CEPLG
+#' COMESA
+#' EAC
+#' ECOWAS
+#' IGAD
+#' IOC
+#' MRU
+#' SACU
+#' UEMOA
+#' UMA
+#' PALOP
+#' PARTA
+#' CACM
+#' EurAsEC
+#' Agadir
+#' SAARC
+#' ASEAN
+#' NAFTA
+#' GCC
+#' CSN
+#' CARICOM
+#' EU
+#' CAN
+#' ACP
+#' Landlocked
+#' AOSIS
+#' SIDS
+#' Islands
+#' LDC
+#' 
+#' Retrieved from the raster package. For further details on that package, see \url{https://CRAN.R-project.org/package=raster}. Original data source: The GADM database of Global Administrative Areas. For further details on that databse, see \url{http://gadm.org/}.
+"moz0"
+
+#' moz1.
+#'
+#' Map of Mozambique at administrative level 1.
+#'
+#' @format A SpatialPolygonsDataFrame with the following variables in the \code{data} slot : 
+#' OBJECTID
+#' ID_0
+#' ISO
+#' NAME_0
+#' ID_1
+#' NAME_1
+#' HASC_1
+#' CCN_1
+#' CCA_1
+#' TYPE_1
+#' ENGTYPE_1
+#' NL_NAME_1
+#' VARNAME_1
+#' 
+#' Retrieved from the raster package. For further details on that package, see \url{https://CRAN.R-project.org/package=raster}. Original data source: The GADM database of Global Administrative Areas. For further details on that databse, see \url{http://gadm.org/}.
+"moz1"
+
+
+#' moz2.
+#'
+#' Map of Mozambique at administrative level 2.
+#'
+#' @format A SpatialPolygonsDataFrame with the following variables in the \code{data} slot : 
+#' OBJECTID
+#' ID_0
+#' ISO
+#' NAME_0
+#' ID_1
+#' NAME_1
+#' ID_2
+#' NAME_2
+#' HASC_2
+#' CCN_2
+#' CCA_2
+#' TYPE_2
+#' ENGTYPE_2
+#' NL_NAME_2
+#' VARNAME_2
+#' 
+#' Retrieved from the raster package. For further details on that package, see \url{https://CRAN.R-project.org/package=raster}. Original data source: The GADM database of Global Administrative Areas. For further details on that databse, see \url{http://gadm.org/}.
+"moz2"
+
+
+#' moz3.
+#'
+#' Map of Mozambique at administrative level 3.
+#'
+#' @format A SpatialPolygonsDataFrame with the following variables in the \code{data} slot : 
+#' OBJECTID
+#' ID_0
+#' ISO
+#' NAME_0
+#' ID_1
+#' NAME_1
+#' ID_2
+#' NAME_2
+#' ID_3
+#' NAME_3
+#' CCN_3
+#' CCA_3
+#' TYPE_3
+#' ENGTYPE_3
+#' NL_NAME_3
+#' VARNAME_3
+#'  
+#' Retrieved from the raster package. For further details on that package, see \url{https://CRAN.R-project.org/package=raster}. Original data source: The GADM database of Global Administrative Areas. For further details on that databse, see \url{http://gadm.org/}.
+"moz3"
+
+
+#' man2.
+#'
+#' Map of the Manhiça district at administrative level 2.
+#'
+#' @format A SpatialPolygonsDataFrame subset of \code{moz2}
+"man2"
+
+#' man3.
+#'
+#' Map of the Manhiça district at administrative level 3.
+#'
+#' @format A SpatialPolygonsDataFrame subset of \code{moz3}
+"man3"
+
+
+#' mag2.
+#'
+#' Map of the Magude district at administrative level 2.
+#'
+#' @format A SpatialPolygonsDataFrame subset of \code{moz2}
+"mag2"
+
+#' mag3
+#'
+#' Map of the Magude district at administrative level 3.
+#'
+#' @format A SpatialPolygonsDataFrame subset of \code{moz3}
+"mag3"
+
+
+#' mop2.
+#'
+#' Map of the Mopeia district at administrative level 2.
+#'
+#' @format A SpatialPolygonsDataFrame subset of \code{moz2}
+"mop2"
+
+#' mop3
+#'
+#' Map of the Mopeia district at administrative level 3.
+#'
+#' @format A SpatialPolygonsDataFrame subset of \code{moz3}
+"mop3"
+
+
+#' moz0_fortified
+#' 
+#' A "fortified" version of \code{moz0}
+#' 
+#' @format A dataframe created through \code{ggplot2::fortify}
+"moz0_fortified"
+
+#' moz1_fortified
+#' 
+#' A "fortified" version of \code{moz1}
+#' 
+#' @format A dataframe created through \code{ggplot2::fortify}
+"moz1_fortified"
+
+
+#' moz2_fortified
+#' 
+#' A "fortified" version of \code{moz2}
+#' 
+#' @format A dataframe created through \code{ggplot2::fortify}
+"moz2_fortified"
+
+
+#' moz3_fortified
+#' 
+#' A "fortified" version of \code{moz3}
+#' 
+#' @format A dataframe created through \code{ggplot2::fortify}
+"moz3_fortified"
+
+
+
+#' man2_fortified
+#' 
+#' A "fortified" version of \code{man2}
+#' 
+#' @format A dataframe created through \code{ggplot2::fortify}
+"man2_fortified"
+
+
+#' man3_fortified
+#' 
+#' A "fortified" version of \code{man3}
+#' 
+#' @format A dataframe created through \code{ggplot2::fortify}
+"man3_fortified"
+
+
+
+#' mag2_fortified
+#' 
+#' A "fortified" version of \code{mag2}
+#' 
+#' @format A dataframe created through \code{ggplot2::fortify}
+"mag2_fortified"
+
+
+#' mag3_fortified
+#' 
+#' A "fortified" version of \code{mag3}
+#' 
+#' @format A dataframe created through \code{ggplot2::fortify}
+"mag3_fortified"
+
+
+
+#' mop2_fortified
+#' 
+#' A "fortified" version of \code{mop2}
+#' 
+#' @format A dataframe created through \code{ggplot2::fortify}
+"mop2_fortified"
+
+
+#' mop3_fortified
+#' 
+#' A "fortified" version of \code{mop3}
+#' 
+#' @format A dataframe created through \code{ggplot2::fortify}
+"mop3_fortified"
