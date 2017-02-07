6e0640e5908768d8ae39f6f1ff1f3c397a782344
Joe Brew <joebrew@gmail.com>
2017-02-07 10:33:19 +0200

    documentation, etc

---
+++ R/fuzzy_match.R
@@ -0,0 +1,47 @@
+#' Fuzzy match
+#'
+#' Use approximate string matching to assign "similarity" scores to
+#' potentially mis-entered data
+#' @param x A character string
+#' @param y A character string with which to match
+#' if \code{NULL}, x will be used
+#' @export
+
+fuzzy_match <- function(x,
+                        y = NULL){
+
+  # Packages
+  require(stringdist)
+
+  # If no y, use x
+  if(is.null(y)){
+    y <- x
+  }
+
+  # Make evrything lower case
+  x <- tolower(x)
+  y <- tolower(y)
+
+  # Generate placeholder for results
+  results_matrix <- matrix(NA,
+                           nrow = length(x),
+                           ncol = length(y))
+
+  # Calculate distance by row
+  for (i in 1:nrow(results_matrix)){
+    distance <- stringdist(a = x[i],
+                           b = y,
+                           method = 'jw')
+    results_matrix[i,] <- distance
+  }
+
+  # Give dimension names
+  dnames <- list(x = x,
+                 y = y)
+  dimnames(results_matrix) <- dnames
+
+  # Return the results
+  return(results_matrix)
+}
+
+
---
+++ R/get_dictionary.R
@@ -0,0 +1,47 @@
+#' get dictionary
+#' 
+#' Get a dictionary for the following databases:
+#' 1. Manhica (dssodk) with the following tables
+#' HOUSEHOLD_ECONOMICS_CORE, 
+#' LOCATION_DETAILS_CORE,
+#' INDIVIDUAL_DETAILS_CORE
+#' 2. Magude (round 1) with the following tables
+#' 3. Magude (round 2) with the following tables
+#' @param questions If \code{TRUE}, will return a dictionary of 
+#' census questions;
+#' the alternative (\code{FALSE}) is to return the dictionary of the answers
+#' @param manhica If \code{TRUE}, will return a dictionary for manhica;
+#' the alternative (\code{FALSE}) is to return the dictionary for Magude
+#' @param round Which round of census (only applicable to Manhica)
+#' @export
+
+get_dictionary <- function(questions = TRUE,
+                                  manhica = TRUE,
+                                  round = 1){
+  require(gsheet)
+  
+  if(questions){
+    if(manhica){
+      url <- 'https://docs.google.com/spreadsheets/d/12X6ofKxfqXaEdMC2Jy6EpSqS-SWuUiC4_oBmSWquaV4/edit#gid=0'
+    } else {
+      if(round == 1){
+        url <- 'https://docs.google.com/spreadsheets/d/1Q09pULHEZXa1UErguCDeLrSluXOQTT0yTrFHeAhV8Ho/edit?usp=sharing'
+      } else if(round == 2){
+        url <- 'https://docs.google.com/spreadsheets/d/1j6HIEvtwKypXeGwrVqxwv6joSAT4gd72LIh3S0WhRro/edit?usp=sharing'
+      }
+    }
+  } else {
+    # Answers!
+    if(manhica){
+      url <- 'https://docs.google.com/spreadsheets/d/13w2ZA9zfxbr-Zc4wjooH1ScfeZfw_7_AbSY00RCd-uY/edit#gid=0'
+    } else {
+      if(round == 1){
+        url <- 'https://docs.google.com/spreadsheets/d/1-WuEEEbdlaU7UPsNr8cSio7aJa3d3EUtLH4W8ekGH00/edit?usp=sharing'
+      } else if(round == 2){
+        url <- 'https://docs.google.com/spreadsheets/d/1QsDeDXbqai5jY8Y6HMSiTKjDQiV2gPUzTPDYJHBemao/edit?usp=sharing'
+      }
+    }
+  }
+  dictionary <- gsheet2tbl(url)
+  return(dictionary)
+}
